ddf5c7ea95f99175d995111d34f0112580924e66
Edward Munoz <edzzo456@gmail.com>
2020-11-11 17:57:10 -0800

    created basic level for testing, added collisions and tiles

--- Game.py
+++ Game.py
@@ -4,7 +4,7 @@ import player_enemy_module
 # add sprites to the screen
 all_sprites = pygame.sprite.Group()
 char = player_enemy_module.Player()
-enemy = player_enemy_module.Enemy()
+bad_dudes = player_enemy_module.red_her()
 
 # setting variables for the screen
 width = 1000
@@ -20,7 +20,7 @@ clock = pygame.time.Clock()
 
 # the character
 all_sprites.add(char)
-all_sprites.add(enemy)
+all_sprites.add(bad_dudes)
 
 # map tiles
 grass_tile = pygame.image.load("tile000.png")
@@ -42,10 +42,46 @@ def load_map(path):
 game_map = load_map('tutorial_level')
 
 
+def collision_test(rect, tiles):
+    collide_list = []
+    for tile in tiles:
+        if rect.colliderect(tile):
+            collide_list.append(tile)
+    return collide_list
+
+
+def move(rect, movement, tiles):
+    collision_types = {'top': False, 'bottom': False, 'right': False, 'left': False}
+    rect.x += movement[0]
+    collide_list = collision_test(rect, tiles)
+    for tile in collide_list:
+        if movement[0] > 0:
+            rect.right = tile.left
+            collision_types['right'] = True
+        elif movement[0] < 0:
+            rect.left = tile.right
+            collision_types['left'] = True
+    rect.y += movement[1]
+    collide_list = collision_test(rect, tiles)
+    for tile in collide_list:
+        if movement[1] > 0:
+            rect.bottom = tile.top
+            collision_types['bottom'] = True
+        elif movement[1] < 0:
+            rect.top = tile.bottom
+            collision_types['top'] = True
+
+    return rect, collision_types
+
+
 def game_loop():
     running = True
+    left = False
+    right = False
+    jump = False
+    air_timer = 0
     while running:
-        display.fill((0, 100, 225))
+        display.fill((0, 0, 40))
         clock.tick(FPS)
         tile_rects = []
         y = 0
@@ -63,10 +99,41 @@ def game_loop():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-        if char.in_air:
-            char.speedy += 0.2
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    right = True
+                if event.key == pygame.K_LEFT:
+                    left = True
+                if event.key == pygame.K_UP:
+                    if air_timer < 4:
+                        jump = True
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    right = False
+                if event.key == pygame.K_LEFT:
+                    left = False
+        char.rect, collisions = move(char.rect, char.movement, tile_rects)
+        if air_timer > 4:
+            jump = False
+        if right:
+            char.movement[0] = 4
+        elif left:
+            char.movement[0] = -4
         else:
+            char.movement[0] = 0
+        char.movement[1] += char.speedy
+        if jump:
+            char.movement[1] = -5
+        char.speedy += 0.2
+        if char.movement[1] >= 5:
+            char.movement[1] = 5
+        if collisions['bottom']:
             char.speedy = 0
+            air_timer = 0
+        elif collisions['top']:
+            char.movement[1] = -char.movement[1]
+        else:
+            air_timer += 1
         all_sprites.update()
         all_sprites.draw(display)
         surface = pygame.transform.scale(display, (width, height))
--- player_enemy_module.py
+++ player_enemy_module.py
@@ -5,7 +5,8 @@ width = 500
 height = 500
 sprite1 = pygame.Surface((16, 16))
 sprite2 = pygame.Surface((16, 16))
-
+left = False
+right = False
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
@@ -14,49 +15,33 @@ class Player(pygame.sprite.Sprite):
         self.image.fill((225, 225, 225))
         self.rect = self.image.get_rect()
         self.rect.centerx = width / 2
-        self.rect.bottom = height - 10
+        self.rect.bottom = 200
         self.speedx = 0
         self.speedy = 0
-        self.in_air = False
-        self.jumped = False
+        self.movement = [0,0]
+
 
     def update(self):
+        self.movement[0] += self.speedx
+        self.movement[1] += self.speedy
         self.speedx = 0
-        keystate = pygame.key.get_pressed()
-        if keystate[pygame.K_LEFT]:
-            self.speedx = -8
-        if keystate[pygame.K_RIGHT]:
-            self.speedx = 8
-        self.rect.x += self.speedx
-        if self.rect.right > width:
+        self.speedy = 0
+        if self.rect.right >= width:
             self.rect.right = width
-        if self.rect.left < 0:
+        if self.rect.left <= 0:
             self.rect.left = 0
-        if keystate[pygame.K_UP]:
-            if not self.jumped:
-                self.speedy = -8
-            else:
-                pass
-        if keystate[pygame.K_DOWN]:
-            self.speedy = 8
-        self.rect.y += self.speedy
-        if self.rect.top > height - 16:
-            self.rect.top = height - 16
-            self.speedy = 0
-            self.in_air = False
-            self.jumped = False
-        else:
-            self.in_air = True
-            self.jumped = True
-        if self.rect.bottom < height:
+        if self.rect.bottom >= height:
             self.rect.bottom = height
+        if self.rect.top <= 0:
+            self.rect.top = 0
 
 
-class Enemy(pygame.sprite.Sprite):
+
+class red_her(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         self.image = sprite2
         self.image.fill((225, 0, 0))
         self.rect = self.image.get_rect()
-        self.rect.centerx = random.randint(20, 240)
+        self.rect.centerx = random.randint(0 * 16, 32 * 16)
         self.rect.bottom = random.randint(100, 240)
